8084c7141699b292ab35cfdcaef77f072ca8eb4d
Giancarlo Malfate Caprino <gianmalfate@gmail.com>
2022-10-16 02:05:25 -0300

    Update pilha.h

--- pilha.h
+++ pilha.h
@@ -1,3 +1,9 @@
+//pila ou filha
+//Giancarlo Malfate Caprino, nUSP: 12725025
+//Henrique Gualberto Marques, nUSP: 13692380
+//Pedro Henrique Cruz da Silva, nUSP: 11833236
+//Rafael Scalon Peres Conti, nUSP: 11871181
+
 #include "fila.h"
 
 #ifndef PILHA_H
@@ -28,7 +34,7 @@ void criar_pilha(t_pilha *pilha);
 int empilhar(t_pilha *pilha, t_elemento elemento);
 int desempilhar(t_pilha *pilha);
 t_no topo(t_pilha *pilha);
-int vazia(t_pilha *pilha);
+int vazia_pilha(t_pilha *pilha);
 void limpa_pilha(t_pilha *pilha);
 
 #endif
